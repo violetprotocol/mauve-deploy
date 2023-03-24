e3a8190f4f0d00bcf6027c7f07b7e146d877b47c
Gabriel Ferraz <sudoferraz@gmail.com>
2023-03-24 12:52:25 +0200

    Adds mint functionality

--- src/scripts/mint.ts
+++ src/scripts/mint.ts
@@ -1,6 +1,10 @@
-import { BigNumber, ethers } from "ethers";
-import { FeeAmount } from './constants'
+import { Domain } from "@violetprotocol/ethereum-access-token-helpers/dist/messages";
+import { AccessTokenVerifier } from "@violetprotocol/ethereum-access-token-helpers/dist/types";
+import { BigNumber, ethers, Wallet } from "ethers";
+import { FeeAmount, TICK_SPACINGS } from './constants'
 import { encodePriceSqrt } from "./encodePriceSqrt";
+import { generateAccessTokenForMulticall } from "./generateAccessToken";
+import { getMaxTick, getMinTick } from "./ticks";
 
 export type CreatePoolIfNecessary = (
   token0: string,
@@ -10,11 +14,21 @@ export type CreatePoolIfNecessary = (
   value?: { value: number }
 ) => Promise<string>
 
+export type MintedResults = {
+  tokenId: number
+  liquidity: number,
+  amount0: number,
+  amount1: number,
+}
+
 export const mint = async (
     nonFungiblePositionManager: ethers.Contract,
     tokens: ethers.Contract[],
-    createAndInitializePoolIfNecessary: CreatePoolIfNecessary
-) => {
+    createAndInitializePoolIfNecessary: CreatePoolIfNecessary,
+    addressToMintFrom: ethers.Wallet,
+    signer: Wallet,
+    domain: Domain,
+): Promise<MintedResults>  => {
       await createAndInitializePoolIfNecessary(
         tokens[0].address,
         tokens[1].address,
@@ -22,59 +36,42 @@ export const mint = async (
         encodePriceSqrt(1, 1)
       )
 
+      const mintParams = {
+        token0: tokens[0].address,
+        token1: tokens[1].address,
+        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        fee: FeeAmount.MEDIUM,
+        recipient: addressToMintFrom.address,
+        amount0Desired: 15,
+        amount1Desired: 15,
+        amount0Min: 0,
+        amount1Min: 0,
+        deadline: 10,
+      }
+      const mintMulticallParameters = [nonFungiblePositionManager.interface.encodeFunctionData('mint', [mintParams])]
+      const { eat, expiry } = await generateAccessTokenForMulticall(
+        signer,
+        domain,
+        addressToMintFrom,
+        nonFungiblePositionManager,
+        mintMulticallParameters
+      )
+      const txData = await nonFungiblePositionManager['multicall(uint8,bytes32,bytes32,uint256,bytes[])'](
+        eat.v,
+        eat.r,
+        eat.s,
+        expiry,
+        mintMulticallParameters
+      );
+      const { tokenId: tokenId, liquidity: liquidity, amount0: amount0, amount1: amount1 } = nonFungiblePositionManager.interface.decodeFunctionResult('collect', txData)
+      const mintedResults: MintedResults = {
+        tokenId,
+        liquidity,
+        amount0,
+        amount1
+      }
+
+      return mintedResults;
+
 }
-    // it('creates a token', async () => {
-    //
-    //   const mintParams = {
-    //     token0: tokens[0].address,
-    //     token1: tokens[1].address,
-    //     tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-    //     tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
-    //     fee: FeeAmount.MEDIUM,
-    //     recipient: other.address,
-    //     amount0Desired: 15,
-    //     amount1Desired: 15,
-    //     amount0Min: 0,
-    //     amount1Min: 0,
-    //     deadline: 10,
-    //   }
-    //   const mintMulticallParameters = [nft.interface.encodeFunctionData('mint', [mintParams])]
-    //   const { eat, expiry } = await generateAccessTokenForMulticall(
-    //     signer,
-    //     domain,
-    //     wallet,
-    //     nft,
-    //     mintMulticallParameters
-    //   )
-    //
-    //   await nft['multicall(uint8,bytes32,bytes32,uint256,bytes[])'](
-    //     eat.v,
-    //     eat.r,
-    //     eat.s,
-    //     expiry,
-    //     mintMulticallParameters
-    //   )
-    //   expect(await nft.balanceOf(other.address)).to.eq(1)
-    //   expect(await nft.tokenOfOwnerByIndex(other.address, 0)).to.eq(1)
-    //   const {
-    //     fee,
-    //     token0,
-    //     token1,
-    //     tickLower,
-    //     tickUpper,
-    //     liquidity,
-    //     tokensOwed0,
-    //     tokensOwed1,
-    //     feeGrowthInside0LastX128,
-    //     feeGrowthInside1LastX128,
-    //   } = await nft.positions(1)
-    //   expect(token0).to.eq(tokens[0].address)
-    //   expect(token1).to.eq(tokens[1].address)
-    //   expect(fee).to.eq(FeeAmount.MEDIUM)
-    //   expect(tickLower).to.eq(getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
-    //   expect(tickUpper).to.eq(getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]))
-    //   expect(liquidity).to.eq(15)
-    //   expect(tokensOwed0).to.eq(0)
-    //   expect(tokensOwed1).to.eq(0)
-    //   expect(feeGrowthInside0LastX128).to.eq(0)
-    //   expect(feeGrowthInside1LastX128).to.eq(0)
