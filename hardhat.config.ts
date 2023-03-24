f89a44df7207bd62b99104e0cf745b3dab68840d
Papa Smurf <chris@violet.co>
2023-03-24 09:49:53 +0000

    feat: added oz, EAT dependencies and new hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,69 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-typechain";
+import "hardhat-watcher";
+import "hardhat-dependency-compiler";
+import "hardhat-contract-sizer";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+export default {
+  networks: {
+    localhost: {
+      url: `https://localhost:8545`,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrumRinkeby: {
+      url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimismKovan: {
+      url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  contractSizer: {
+    runOnCompile: false,
+  },
+  solidity: {
+    compilers: ["0.7.6"],
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: "test", params: { testFiles: ["{path}"] } }],
+      files: ["./test/**/*"],
+      verbose: true,
+    },
+  },
+  dependencyCompiler: {
+    paths: [
+      "@violetprotocol/ethereum-access-token/contracts/AccessTokenVerifier.sol",
+      "@openzeppelin/contracts/token/ERC20/ERC20.sol",
+    ],
+  },
 };
-
-export default config;
