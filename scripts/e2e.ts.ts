8386e12a0994c1d81b30f1c38463e94e054699a7
Papa Smurf <chris@violet.co>
2023-03-24 09:42:09 +0000

    feat: script skeleton and imports

---
+++ scripts/e2e.ts.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+import { deployEAT } from "./deployEAT";
+import { deployERC20s } from "./deployERC20s";
+import { deployMauve } from "./deployMauve";
+import { deployPool } from "./deployPool";
+import { mintPosition } from "./mintPosition";
+import { performSwap } from "./performSwap";
+
+async function main() {
+  await deployEAT();
+  await deployERC20s();
+  await deployMauve();
+  await deployPool();
+  await mintPosition();
+  await performSwap();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
