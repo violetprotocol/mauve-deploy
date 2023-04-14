5f455d19ff7a3d0d31cf56d3e3056aab9970a100
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-04-14 14:07:31 +0100

    update outdated task

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { task } from "hardhat/config";
+// import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "./type-extensions";
 
-import Table from "cli-table3";
+// import Table from "cli-table3";
 
-import { MauveDeployer } from "./deployer/MauveDeployer";
+// TODO: Update this to use `deployMauve()` if we want to expose it as a task
 
-task("deploy-mauve", "Deploys Mauve contracts", async (args, hre) => {
-  const [actor] = await hre.ethers.getSigners();
-  const contracts = await MauveDeployer.deploy(actor);
+// task("deploy-mauve", "Deploys Mauve contracts", async (args, hre) => {
+//   const [actor] = await hre.ethers.getSigners();
+//   const contracts = await MauveDeployer.deploy(actor, VIOLET_ID_ADDRESS);
 
-  const table = new Table({
-    head: ["Contract", "Address"],
-    style: { border: [] },
-  });
-  for (const item of Object.keys(contracts)) {
-    table.push([item, contracts[item].address]);
-  }
-  console.info(table.toString());
-});
+//   const table = new Table({
+//     head: ["Contract", "Address"],
+//     style: { border: [] },
+//   });
+//   for (const item of Object.keys(contracts)) {
+//     table.push([item, contracts[item].address]);
+//   }
+//   console.info(table.toString());
+// });
