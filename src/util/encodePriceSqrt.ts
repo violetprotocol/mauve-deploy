023aded8d71655e3d9fd92412412cf05c55390ed
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-04-17 14:40:16 +0100

    add comment

--- src/util/encodePriceSqrt.ts
+++ src/util/encodePriceSqrt.ts
@@ -4,6 +4,7 @@ import { BigNumber, BigNumberish } from "ethers";
 bn.config({ EXPONENTIAL_AT: 999999, DECIMAL_PLACES: 40 });
 
 // returns the sqrt price as a 64x96
+// TODO: Update this to take into account decimal places of reserve1 and reserve0
 export function encodePriceSqrt(
   reserve1: BigNumberish,
   reserve0: BigNumberish
