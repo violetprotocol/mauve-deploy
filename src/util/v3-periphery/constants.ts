b62d2a901ba7af5cdfb24f1dc5db15cf202c46ea
Gabriel Ferraz <sudoferraz@gmail.com>
2023-09-12 21:00:09 +0200

    Removes references to uniswap and changelog (#16)

--- src/util/v3-periphery/constants.ts
+++ src/util/v3-periphery/constants.ts
@@ -1,7 +1,3 @@
-/*
-https://github.com/Uniswap/uniswap-v3-periphery/blob/e3fb908f1fbc72f1b1342c983c9ad756448c3bba/test/shared/constants.ts
-*/
-
 import { BigNumber } from 'ethers'
 
 export const MaxUint128 = BigNumber.from(2).pow(128).sub(1)
--- src/util/v3-periphery/ticks.ts
+++ src/util/v3-periphery/ticks.ts
@@ -1,5 +1,3 @@
-/* https://github.com/Uniswap/uniswap-v3-periphery/blob/e3fb908f1fbc72f1b1342c983c9ad756448c3bba/test/shared/ticks.ts */
-
 import { BigNumber } from 'ethers'
 
 export const getMinTick = (tickSpacing: number) => Math.ceil(-887272 / tickSpacing) * tickSpacing
